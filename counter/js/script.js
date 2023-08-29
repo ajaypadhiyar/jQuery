aa699d0fceb09082f57b4a8cb48b46346006cb81
Ajay Padhiyar <139475530+ajaypadhiyar@users.noreply.github.com>
2023-08-29 11:49:50 +0530

    Update script.js

--- counter/js/script.js
+++ counter/js/script.js
@@ -32,6 +32,29 @@ function startCounter() {
   }
 
 
+ // number & coma(,) counter
+    a = 0;
+    $(window).scroll(function() {
+        var oTop = $(".rank__counter").offset().top -
+            window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+            $('.count').each(function() {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 2700,
+                    easing: 'swing',
+                    step: function(now) {
+                        // $(this).text(Math.ceil(now));
+                        now = Number(Math.ceil(now)).toLocaleString('en');
+                        $(this).text(now);
+                    }
+                });
+            });
+            a = 1;
+        }
+    });
+
 a = 0;
 $(window).scroll(function() {
     var oTop = $(".love_counter").offset().top -
