cf6fbe339cb36fdfc29588422c642931e0e7e244
Ajay Padhiyar <139475530+ajaypadhiyar@users.noreply.github.com>
2023-09-15 10:55:36 +0530

    Update script.jscounter

--- counter/js/script.js
+++ counter/js/script.js
@@ -75,3 +75,40 @@ $(window).scroll(function() {
     }
 });
 
+
+$(window).scroll(startCounter);
+
+function startCounter() {
+  var oTop = $(".about_bg").offset().top - window.innerHeight;
+  $(window).off("scroll", startCounter);
+  $(".counter").each(function () {
+    var $this = $(this);
+    jQuery({
+      Counter: 0,
+    }).animate(
+      {
+        Counter: $this.text().replace(/,/g, ""),
+      },
+      {
+        duration: 3000,
+        easing: "swing",
+        step: function () {
+          $this.text(commaSeparateNumber(Math.floor(this.Counter)));
+        },
+        complete: function () {
+          $this.text(commaSeparateNumber(this.Counter));
+          // alert('finished');
+        },
+      }
+    );
+  });
+
+  function commaSeparateNumber(val) {
+    while (/(\d+)(\d{3})/.test(val.toString())) {
+      val = val.toString().replace(/(\d+)(\d{3})/, "$1" + "," + "$2");
+    }
+    return val;
+  }
+}
+
+
